9fdbd4571491148f44f9b3073bd551f28a6b4d78
sagarpipaliya <70269480+sagarpipaliya@users.noreply.github.com>
2020-10-09 14:09:59 +0530

    Update view_student.php

--- view_student.php
+++ view_student.php
@@ -1,6 +1,6 @@
 <?php 
 	$con = mysqli_connect('studentdb.c9dqyjiw7mfq.us-east-1.rds.amazonaws.com','admin123','admin123','studentdb','3306');
-	$qry="select * from stud";
+	$qry="select * from stud order by sname desc";
 	$res=mysqli_query($con,$qry);
 ?>
 <!DOCTYPE html>
